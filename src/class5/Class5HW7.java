5b8872ef936bbc56bb8d1f348d8c0cb38d61e74d
shahriarbsc <shahriar.bsc@gmail.com>
2023-01-11 22:06:10 -0500

    class 7 code

---
+++ src/class5/Class5HW7.java
@@ -0,0 +1,39 @@
+package class5;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Class5HW7 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("please enter your country");
+        String country = input.next();
+
+
+        String language = "English";
+        String language1 = "Bengali";
+        String language2 = "Urdu";
+        String language3 = "Hindi";
+        String language4 = "Afghani";
+        switch (country) {
+
+         /*   case "USA":
+                System.out.println("You speak English");
+                break;
+            case "BANGLADESH":
+                System.out.println("You speak Bengali");
+                break;
+            case "Pakistan":
+                System.out.println("You speak Urdu");
+                break;
+            case "USA":
+                System.out.println("You speak English");
+                break;*/
+
+
+        }
+
+
+    }
+}
